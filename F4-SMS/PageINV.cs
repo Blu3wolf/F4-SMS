548bb69576cd64a798fbba4f87bc98eb8ad49f13
Blue <blue@blu3wolf.com>
2017-07-18 12:46:09 +0800

    define object constructor and required abstract members for class

--- F4-SMS/PageINV.cs
+++ F4-SMS/PageINV.cs
@@ -9,7 +9,21 @@ namespace F4SMS
 	/* responsible for telling Display how to draw INV page.
 	 * knows the logic for how the INV page reacts to user input. */
 
-	class PageINV
+	class PageINV : SMSPage
 	{
+		PageINV()
+		{
+
+		}
+
+		public override void DrawMe()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void ButtonPress(int OSB)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
